555236f992f7012a8d44211b8ee792da038b4c64
Taras Poiatsyka <tvsxar@MacBook-Air---Taras.local>
2025-01-16 23:19:38 +0200

    header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import './header.css'
+import logo from '../../img/icons/logo.svg'
+
+function Header() {
+    return (
+        <header className='header'>
+            <div className='container'>
+                <div className='header-row'>
+                    <div className='header-logo'>
+                        <img src={logo} alt='logo' />
+                        <span>FASHION</span>
+                    </div>
+                    <nav className='header-nav'>
+                        <ul>
+                            <li><a href='#!'>CATALOGUE</a></li>
+                            <li><a href='#!'>FASHION</a></li>
+                            <li><a href='#!'>FAVOURITE</a></li>
+                            <li><a href='#!'>LIFESTYLE</a></li>
+                            <li><a href='#!' className='header-nav-btn'>SIGN IN</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/promo/Promo.jsx
@@ -0,0 +1,22 @@
+import './promo.css';
+import promoImg from '../../img/images/header-img.jpg'
+
+function Promo() {
+    return (
+        <section className="promo">
+            <div className="container">
+                <div className="promo-content">
+                    <div className="promo-text">
+                        <div className="promo-title"><span className='highlight'><span>LET’S</span></span>
+                         EXPLORE <span className='highlight highlight--yellow'><span>UNIQUE</span></span> CLOTHES.</div>
+                        <div className="promo-desc">Live for Influential and Innovative fashion!</div>
+                        <div className="promo-btn-wrapper"><a href='#!' className='promo-btn'>Shop Now</a></div>
+                    </div>
+                    <div className="promo-img"><img src={promoImg} /></div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Promo;
